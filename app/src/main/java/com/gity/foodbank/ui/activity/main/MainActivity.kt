f927715b1d8f89a2cce02a13ecbbfecda659f64b
Yoki Jati Perkasa <yokijati@gmail.com>
2023-12-30 02:19:03 +0700

    Testing GIT Reset

--- app/src/main/java/com/gity/foodbank/ui/activity/main/MainActivity.kt
+++ app/src/main/java/com/gity/foodbank/ui/activity/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.gity.foodbank
+package com.gity.foodbank.ui.activity.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.gity.foodbank.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,10 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        for (i in 1..5) {
+            Toast.makeText(this, "Hello World $i", Toast.LENGTH_SHORT).show()
+        }
 
 
 
